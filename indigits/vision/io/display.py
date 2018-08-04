0945cfdbbe77c3c80982fb695375d121dc89ace3
Shailesh Kumar <shailesh@indigits.com>
2018-08-04 22:27:04 +0530

    support for displaying multiple images in one go

--- indigits/vision/io/display.py
+++ indigits/vision/io/display.py
@@ -9,18 +9,25 @@ class DisplayManager:
     Manages the display of one or more OpenCV 
     windows
     '''
-    def __init__(self, window_names):
+    def __init__(self, window_names, gap_x=100):
         if isinstance(window_names, str):
             window_names = [window_names]
         self.window_names = window_names
         for (i, window_name) in enumerate(self.window_names):
             cv2.namedWindow(window_name)
-            cv2.moveWindow(window_name, i*10, i*10)
+            cv2.moveWindow(window_name, i*gap_x, i*10)
 
-    def show(self, frame, window_num=0):
+    def show(self, image, window_num=0):
         '''Shows a frame on a specified window'''
         window_name = self.window_names[window_num]
-        cv2.imshow(window_name, frame)
+        cv2.imshow(window_name, image)
+
+    def show_all(self, *images):
+        '''Shows multiple images'''
+        for i, image in enumerate(images):
+            window_name = self.window_names[i]
+            cv2.imshow(window_name, image)
+
 
     def select_roi(self, frame, window_num=0):
         '''Selects a region of interest in a given frame'''
