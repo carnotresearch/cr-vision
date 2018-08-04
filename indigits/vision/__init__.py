ffa18fbe6812aab7e30bc55d5cb8735f01a18e02
Shailesh Kumar <shailesh@indigits.com>
2018-08-04 14:14:15 +0530

    addition of new sub-packages

--- indigits/vision/__init__.py
+++ indigits/vision/__init__.py
@@ -3,9 +3,14 @@ indigits.vision package
 '''
 # pylint: disable=W0401
 from .core import *
+from .misc import *
 from .geom_transform import *
 from .filters import *
 from .edits import *
 from .noise import *
 from .apps import *
 from .errors import *
+from .concurrent import *
+from .motion import  *
+from .io import *
+from .object_tracker import *
