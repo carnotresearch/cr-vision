cc4e2ba0c277b434ba6ae288cc37b0bab0cfd389
Shailesh Kumar <shailesh@indigits.com>
2018-08-05 21:45:59 +0530

    skip bgr to gray conversion if the image is gray level

--- indigits/vision/core/cvt_color.py
+++ indigits/vision/core/cvt_color.py
@@ -5,6 +5,12 @@ import cv2
 
 def bgr_to_gray(image):
     '''Converts from BGR to RGB'''
+    if image.ndim == 2:
+        # it is already a gray scale image
+        return image
+    if image.shape[2] == 1:
+        # it is already a gray scale image
+        return image
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 
