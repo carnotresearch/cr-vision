5d7846d880883452f3127c300407f0db1a58f430
Shailesh Kumar <shailesh@indigits.com>
2018-08-06 23:50:21 +0530

    constrast enhancement, pixelization, cartoonization

--- indigits/vision/core/effects.py
+++ indigits/vision/core/effects.py
@@ -126,7 +126,7 @@ EMBOSS_FILTERS = {
     'default': np.array([[-2, -1, 0],
                          [-1, 1, 1],
                          [0, 1, 2]
-                         ]) /3,
+                         ]) / 3,
     'SW': np.array([[0, -1, -1],
                     [1, 0, -1],
                     [1, 1, 0]]),
@@ -195,47 +195,6 @@ def sharpen(image):
     return sharpened_image
 
 
-def vignette(image, vignette_size=100):
-    '''
-    Applies vignette effect on an image
-
-    Args:
-        image (array): Input image
-
-    Returns:
-        Vignetted image
-    '''
-    # height and width of the image
-    height, width = image.shape[:2]
-    # We will construct the vignette mask using Gaussian kernels
-    # Construct a Gaussian kernel for x-direction
-    kernel_x = cv2.getGaussianKernel(width, vignette_size)
-    # Construct a Gaussian kernel for y-direction
-    kernel_y = cv2.getGaussianKernel(height, vignette_size)
-    # Multiply them together to form a combined Gaussian kernel
-    kernel = kernel_x*kernel_y.T
-    # compute the norm of the Gaussian kernel
-    kernel_norm = np.linalg.norm(kernel)
-    # prepare the mask
-    mask = 255 * kernel / kernel_norm
-    if image.ndim == 2:
-        # it's a black and white images
-        output = image*mask
-    else:
-        channels = image.shape[2]
-        if channels == 1:
-            # it's a black and white image
-            output = image*mask
-        else:
-            # it's a BGR image
-            b,g,r = cv2.split(image)
-            b = b*mask
-            g = g*mask
-            r = r*mask
-            output = cv2.merge(b,g,r)
-    return output
-
-
 def vignette(image, vignette_size=150):
     '''
     Applies vignette effect on an image
@@ -273,3 +232,84 @@ def vignette(image, vignette_size=150):
             r = r*mask
             output = cv2.merge([b, g, r])
     return output.astype('uint8')
+
+
+def enhance_contrast(image):
+    '''
+    Applies vignette effect on an image
+
+    Args:
+        image (array): Input image
+
+    Returns:
+        Image with enhanced contrast
+    '''
+    if iv.is_gray_scale(image):
+        # the image itself is gray scale
+        y = image
+    elif iv.is_3channel(image):
+        # Convert from BGR to YUV format
+        image_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+        # Extract Y channel
+        y = image_yuv[:, :, 0]
+    else:
+        raise iv.IVError("Invalid image format")
+    # perform histogram equalization on the grayscale y channel
+    y = cv2.equalizeHist(y)
+    if iv.is_gray_scale(image):
+        # The result is y itself
+        result = y
+    else:
+        # Put back equalized Y channel in image yuv
+        image_yuv[:, :, 0] = y
+        # convert back to BGR format
+        result = cv2.cvtColor(image_yuv, cv2.COLOR_YUV2BGR)
+    return result
+
+
+def cartoonize(image, kernel_size=5):
+    '''
+    Constructs a black and white cartoon version of image
+
+    Args:
+        image (array): Input image
+
+    Returns:
+        Cartoonized image
+    '''
+    #  Convert to gray scale
+    if iv.is_3channel(image):
+        image = iv.bgr_to_gray(image)
+    # Apply median filtering
+    image = cv2.medianBlur(image, 7)
+    # Detect edges in the image
+    edges = cv2.Laplacian(image, cv2.CV_8U, ksize=kernel_size)
+    # Edges are where the Laplacian is small
+    # We prepare an edge mask for small values
+    mask = iv.threshold_below(edges, 100)
+    # We need to do some erosion to merge black edge points
+    # prepare the structuring element
+    se = np.ones((3, 3), np.uint8)
+    # perform erosion
+    mask = cv2.erode(mask, se, iterations=1)
+    # get rid of isolated edge points again
+    mask = cv2.medianBlur(mask, ksize=5)
+    # This is our cartoonized image
+    return mask
+
+
+def pixelize(image, pixel_size=8):
+    '''Pixelizes an image to given pixel size
+
+    Args:
+        image (array): Input image
+
+    Returns:
+        Pixelized image
+    '''
+    factor = 1.0 / pixel_size
+    image = cv2.resize(image, (0, 0), fx=factor, fy=factor,
+                       interpolation=cv2.INTER_NEAREST)
+    image = cv2.resize(image, (0, 0), fx=pixel_size, fy=pixel_size,
+                       interpolation=cv2.INTER_NEAREST)
+    return image
