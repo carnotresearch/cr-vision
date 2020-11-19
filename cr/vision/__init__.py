2f93321ec935006a074ed4cfe2fe39a4cc757b90
Shailesh Kumar <shailesh@indigits.com>
2020-11-19 14:57:52 +0530

    additional packages

--- cr/vision/__init__.py
+++ cr/vision/__init__.py
@@ -2,25 +2,46 @@
 indigits.vision package
 '''
 # pylint: disable=W0401
+
+# Library setup
+from .setup import *
+# Numpy essentials
+from .matrix import *
+from .vector import *
+from .array import *
+from .errors import *
+
+# IO
+from .io import *
+
+# Basic Image Processing
 from .core import *
+from .core import effects
 from .geom_transform import *
 from .filters import *
 from .edits import *
-from .apps import *
-from .errors import *
 from .concurrent import *
-from .motion import  *
-from .io import *
+
+# Image Quality Measurements
+from . import measure
+
+# Reactive Extensions
+from . import crx
+
+# Video Processing
 from .video import *
+from .motion import  *
+
+# Application Specific Stuff
 from .object_tracker import *
 from .saliency import *
 from .retargeting import *
 from .edits import animation
+from .apps import *
 
-from .core import effects
 
-from . import measure
 from . import traffic
-from . import crx
+from . import faces
+from . import pedestrians
 
-from .setup import *
+
