d7c816d880dae90349cf333b00fd946186d90195
Shailesh Kumar <shailesh@indigits.com>
2020-11-23 11:53:09 +0530

    geom rename

---
+++ cr/vision/geom/__init__.py
@@ -0,0 +1,6 @@
+'''
+Package for functions and classes related to geometric transformations of images
+'''
+# pylint: disable=W0401
+from .projective2d_actions import *
+from .projective2d_matrices import *
