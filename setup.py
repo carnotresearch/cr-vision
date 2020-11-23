b2cd930f7552d3ae9657e3259955f041a72ea8f8
Shailesh Kumar <shailesh@indigits.com>
2020-11-23 07:38:14 +0530

    correct python version and required dependencies.

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
@@ -66,7 +66,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=[ "numpy", "scipy", "matplotlib", 
+    "scikit-image", "opencv-contrib-python",
+    "imageio", "click", "sk-video", "csv", "rx"
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -74,7 +77,7 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         'dev': [ ],
-        'test': [ ],
+        'test': [ "tox"],
     },
 
     # If there are data files included in your packages that need to be
