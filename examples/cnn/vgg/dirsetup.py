e01797fe16913a0cf71c5c2839e2dfc15357fd04
Shailesh Kumar <shailesh@indigits.com>
2020-12-02 17:48:50 +0530

    dirsetup for this examples directory

---
+++ examples/cnn/vgg/dirsetup.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+FILE_PATH = Path(__file__).resolve()
+
+MODULE_DIR = FILE_PATH.parent.parent
+EXAMPLES_DIR = MODULE_DIR.parent
+PACKAGE_DIR = EXAMPLES_DIR.parent
+DATA_DIR = PACKAGE_DIR / 'data'
+IMAGES_DIR = DATA_DIR / 'images'
+VIDEO_DIR = DATA_DIR / 'videos'
