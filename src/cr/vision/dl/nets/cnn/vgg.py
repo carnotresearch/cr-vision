36e102decabc433904c453207fbc94394f90397d
Shailesh Kumar <shailesh@indigits.com>
2020-12-04 19:10:36 +0530

    vgg 19

--- src/cr/vision/dl/nets/cnn/vgg.py
+++ src/cr/vision/dl/nets/cnn/vgg.py
@@ -45,8 +45,18 @@ def cnn_vgg16(img_input):
     net = vgg_block(net, 512, 3, 'block5')    
     return net
 
-def cnn_vgg19():
-    pass
+def cnn_vgg19(img_input):
+    # block 1
+    net = vgg_block(img_input, 64, 2, 'block1')
+    # block 2
+    net = vgg_block(net, 128, 2, 'block2')
+    # block 3
+    net = vgg_block(net, 256, 4, 'block3')
+    # block 4
+    net = vgg_block(net, 512, 4, 'block4')
+    # block 5
+    net = vgg_block(net, 512, 4, 'block5')    
+    return net
 
 
 def top_vgg(cnn, classes=1024):
@@ -101,4 +111,46 @@ def model_vgg16(input_tensor=None,
         resource_path = utils.get_file(resource.name, origin,
             cache_subdir="models", file_hash=resource.file_hash)
         model.load_weights(resource_path)
+    elif weights is not None:
+        model.load_weights(weights)
+    return model
+
+
+def model_vgg19(input_tensor=None, 
+    input_shape=None, 
+    classes=1000,
+    include_top=True,
+    pooling=None,
+    weights=None):
+    # provide a way to compute the default input shape
+    input_shape = deduce_input_shape(input_shape, require_flatten=include_top,
+        weights=weights)
+    if input_tensor is not None:
+        if not backend.is_keras_tensor(input_tensor):
+            raise ValueError("input_tensor must be a Keras layer tensor.")
+        img_input = input_tensor
+    else:
+        img_input =  layers.Input(shape=input_shape)
+    net = cnn_vgg19(img_input)
+    if include_top:
+        # add the top classification network
+        net = top_vgg(net, classes)
+    else:
+        # add global pooling if required
+        net = pool_cnn(net, pooling)
+    if input_tensor is not None:
+        inputs = utils.get_source_inputs(input_tensor)
+    else:
+        inputs = img_input
+    # Create model.
+    model = models.Model(inputs, net, name='vgg19')
+    if weights == 'imagenet':
+        resource = FCHOLLET['VGG19']
+        resource = resource['WITH_TOP'] if include_top else resource['NO_TOP']
+        origin = resource.uri
+        resource_path = utils.get_file(resource.name, origin,
+            cache_subdir="models", file_hash=resource.file_hash)
+        model.load_weights(resource_path)
+    elif weights is not None:
+        model.load_weights(weights)
     return model
