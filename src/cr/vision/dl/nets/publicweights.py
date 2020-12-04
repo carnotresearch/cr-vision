38ad4284c3eb2c00daf15e9d4a6ee6cf9476c0fa
Shailesh Kumar <shailesh@indigits.com>
2020-12-04 19:10:24 +0530

    resnet 50

--- src/cr/vision/dl/nets/publicweights.py
+++ src/cr/vision/dl/nets/publicweights.py
@@ -15,6 +15,7 @@ class WeightFile:
 
 FCHOLLET_ROOT = 'https://github.com/fchollet/deep-learning-models'
 FCHOLLET_V_0_1 = 'releases/download/v0.1/'
+FCHOLLET_V_0_2 = 'releases/download/v0.1/'
 
 
 FCHOLLET = {
@@ -25,5 +26,40 @@ FCHOLLET = {
         "NO_TOP" : WeightFile(root=FCHOLLET_ROOT, folder=FCHOLLET_V_0_1,
             name="vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5",
             file_hash="6d6bbae143d832006294945121d1f1fc")
+    },
+
+    "VGG19" : {
+        "WITH_TOP" : WeightFile(root=FCHOLLET_ROOT, folder=FCHOLLET_V_0_1,
+            name="vgg19_weights_tf_dim_ordering_tf_kernels.h5", 
+            file_hash="cbe5617147190e668d6c5d5026f83318"),  
+        "NO_TOP" : WeightFile(root=FCHOLLET_ROOT, folder=FCHOLLET_V_0_1,
+            name="vgg19_weights_tf_dim_ordering_tf_kernels_notop.h5",
+            file_hash="253f8cb515780f3b799900260a226db6")
+    },
+    "RESNET50" : {
+        "WITH_TOP" : WeightFile(root=FCHOLLET_ROOT, folder=FCHOLLET_V_0_2,
+            name="resnet50_weights_tf_dim_ordering_tf_kernels.h5", 
+            file_hash="a7b3fe01876f51b976af0dea6bc144eb"),  
+        "NO_TOP" : WeightFile(root=FCHOLLET_ROOT, folder=FCHOLLET_V_0_2,
+            name="resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5",
+            file_hash="a268eb855778b3df3c7506639542a6af")
     }
+}
+
+# Model weights hosted by Keras Team
+_KT_ROOT = 'https://github.com/keras-team/keras-applications'
+_KT_RELEASES = 'releases/download'
+_KT_RESNETS = _KT_RELEASES + '/resnet'
+
+
+KERAS_TEAM = {
+    "RESNET50" : {
+        "WITH_TOP" : WeightFile(root=_KT_ROOT, folder=_KT_RESNETS,
+            name="resnet50_weights_tf_dim_ordering_tf_kernels.h5", 
+            file_hash="2cb95161c43110f7111970584f804107"),  
+        "NO_TOP" : WeightFile(root=_KT_ROOT, folder=_KT_RESNETS,
+            name="resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5",
+            file_hash="4d473c1dd8becc155b73f8504c6f6626")
+    },
+    
 }
