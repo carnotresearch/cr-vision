b6e7f055294156839e30633594ab2eec2fc6b6c9
Shailesh Kumar <shailesh@indigits.com>
2021-01-22 17:04:17 +0530

    PSNR

--- experiments/cs/sda/predict.py
+++ experiments/cs/sda/predict.py
@@ -3,15 +3,24 @@ import pathlib
 import sda
 
 from cr.vision.plots import plot_images_with_reconstructions
-
+from skimage.metrics import peak_signal_noise_ratio
 
 rootdir  = r'E:\datasets\vision\birds\CUB_200_2011\images'
 
-images = sda.get_dataset(rootdir)
+images = sda.get_dataset(rootdir, samples=10)
 
 model = sda.load_saved_model()
 print(model.summary())
 
 reconstructions = model.predict(images)
+
+n = images.shape[0]
+
+for i in range(n):
+    src = images[i]
+    dst = reconstructions[i]
+    psnr = peak_signal_noise_ratio(src, dst)
+    print(f"[{i+1}] PSNR : {psnr} dB")
+
 
 plot_images_with_reconstructions(images, reconstructions)
