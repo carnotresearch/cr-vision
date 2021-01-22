4504f78f76708cb8fd4c4cf165d9e1ce8d1ad8bb
Shailesh Kumar <shailesh@indigits.com>
2021-01-22 19:56:51 +0530

    change some defaults

--- experiments/cs/sda/view_data.py
+++ experiments/cs/sda/view_data.py
@@ -1,5 +1,5 @@
 
 import sda
 rootdir  = r'E:\datasets\vision\birds\CUB_200_2011\images'
-images = sda.get_dataset(rootdir)
+images = sda.get_dataset(rootdir, samples=2000, force=True)
 print(images.shape)
