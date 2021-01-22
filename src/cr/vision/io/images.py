bede1406ca97f857f030b945e86e3a30bc469b0e
Shailesh Kumar <shailesh@indigits.com>
2021-01-22 19:53:38 +0530

    make sure that gray scale images are converted to rgb.

--- src/cr/vision/io/images.py
+++ src/cr/vision/io/images.py
@@ -2,12 +2,20 @@ import numpy as np
 import imageio
 
 from cr.vision.core.scaling import resize_crop
+from cr.vision.core.cvt_color import gray_to_rgb
 
+def is_gray(image):
+    return (image.ndim == 2) or (image.ndim == 3 and image.shape[2] == 1)
 
 def read_images(paths, target_width, target_height):
     images = []
     for path in paths:
         image = imageio.imread(path)
+        if is_gray(image):
+            print(f'{path}, {image.shape}')
+            # make sure that image is rgb (and not gray scale)
+            image = gray_to_rgb(image)
+            print(f'shape after conversion {image.shape}')
         image = resize_crop(image, target_width, target_height)
         images.append(image)
     return np.array(images)
