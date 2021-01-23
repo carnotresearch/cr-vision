f42deee8efb59b7b76d828ed680c6f745046e64a
Shailesh Kumar <shailesh@indigits.com>
2021-01-23 13:23:30 +0530

    activation layers added.

--- src/cr/vision/dl/nets/cs/sda.py
+++ src/cr/vision/dl/nets/cs/sda.py
@@ -16,6 +16,7 @@ def cnn_sda(inputs, kernel_initializer="he_normal"):
     patch_size = kernel_size * kernel_size * num_color_channels
     measurements = patch_size / 4
     # note that the sensing layer is not trainable
+    # from N samples in an image patch to M measurements (as channels)
     net = layers.Conv2D(
         filters=measurements, 
         kernel_size=kernel_size, 
@@ -27,20 +28,27 @@ def cnn_sda(inputs, kernel_initializer="he_normal"):
     # each output channel is one measurement
     # the filter applies to whole patch in one go
     # we will now use 1x1 convolutions for following layers
+    # From M measurements (arranged in channels) to N samples (arranged in channels)
     net = layers.Conv2D(patch_size, 1, 
         use_bias=True,
         kernel_initializer=kernel_initializer,
+        activation="relu",
         name="layer-1")(net)
     net = layers.Conv2D(measurements, 1, 
         use_bias=True,
         kernel_initializer=kernel_initializer,
+        activation="relu",
         name="layer-2")(net)
+    # This layer is the inverse of the first layer.
+    # It converts a strip over samples on the
+    # channels dimension into an image patch
     net = layers.Conv2DTranspose(
         filters=num_color_channels, 
         kernel_size=kernel_size,
         strides=strides, 
         use_bias=True,
         kernel_initializer=kernel_initializer,
+        activation="sigmoid",
         name="layer-3")(net)
     return net
 
