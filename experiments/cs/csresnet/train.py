2a0fe6b67ac4b481526e36164861592844e96421
Shailesh Kumar <shailesh@indigits.com>
2021-04-25 00:28:34 +0530

    changed model parameters

--- experiments/cs/csresnet/train.py
+++ experiments/cs/csresnet/train.py
@@ -23,12 +23,18 @@ train_gen = csresnet.augment_training_set(training_set,
 
 # Setup the network to be trained
 input_shape = training_set[0].shape
-
-patch_size = 16
-compression_ratio = 4
-models = csresnet.build_models(input_shape,
+patch_size = 32
+stride_size = 16
+compression_ratio = 64
+num_res_channels=32
+input_shape  = (256, 256, 3)
+print("Building Models.")
+model = csresnet.build_models(input_shape,
     patch_size=patch_size,
+    stride_size=stride_size,
+    num_res_channels=num_res_channels,
     compression_ratio=compression_ratio)
+
 autoencoder = models.autoencoder
 
 
